d2d16240d5560b195143628eeba221359b54fb99
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2023-09-12 00:35:37 +0200

    Renaming

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
   <div class="container-fluid">
 
     <router-link to="/" class="navbar-brand">
-      <img class="img-fluid navbar-img" src="../assets/img/logo.svg">
+      <a class="navbar-brand" href="#">Revenue Distributor Manager</a>
     </router-link>
 
 
@@ -79,58 +79,13 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">About Send2.name</h5>
+        <h5 class="modal-title">About Revenue Distributor Frontend</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
           <span aria-hidden="true"></span>
         </button>
       </div>
       <div class="modal-body">
-        <p>Send2.name is a web3 app that allows you to send tokens to any supported web3 domain name.</p>
-
-        <p>The code is completely <strong>open source</strong> and <a target="_blank" href="https://github.com/send2-name">available on GitHub</a>.</p>
-
-        <p>The list of supported domains:</p>
-
-        <ul>
-          <li><a target="_blank" href="https://ens.domains">.eth (ENS, Ethereum)</a></li>
-          <li><a target="_blank" href="https://punk.domains">.ape (Punk Domains, Polygon PoS Chain)</a></li>
-          <li><a target="_blank" href="https://punk.domains">.arbi (Punk Domains, Arbitrum)</a></li>
-          <li><a target="_blank" href="https://punk.domains">.arbitrum (Punk Domains, Arbitrum)</a></li>
-          <li><a target="_blank" href="https://id.basebook.xyz">.basebook (Basebook, Base)</a></li>
-          <li><a target="_blank" href="https://app.basin.global">.basin (Basin, Polygon PoS Chain)</a></li>
-          <li><a target="_blank" href="https://unstoppabledomains.com/">.bitcoin (Unstoppable Domains, Ethereum)</a></li>
-          <li><a target="_blank" href="https://unstoppabledomains.com/">.blockchain (Unstoppable Domains, Ethereum)</a></li>
-          <li><a target="_blank" href="https://unstoppabledomains.com/">.crypto (Unstoppable Domains, Ethereum)</a></li>
-          <li><a target="_blank" href="https://unstoppabledomains.com/">.dao (Unstoppable Domains, Ethereum)</a></li>
-          <li><a target="_blank" href="https://punk.domains">.degen (Punk Domains, Polygon PoS Chain)</a></li>
-          <li><a target="_blank" href="https://dns.dopewars.gg">.dope (Dope DAO, Optimism)</a></li>
-          <li><a target="_blank" href="https://fantomnames.org">.fantom (Fantom Names, Fantom)</a></li>
-          <li><a target="_blank" href="https://flr.domains">.flr (Flare Domains, Flare Network)</a></li>
-          <li><a target="_blank" href="https://giveth.punk.domains">.giveth (Giveth DAO, Gnosis Chain)</a></li>
-          <li><a target="_blank" href="https://punk.domains">.gnosis (Punk Domains, Gnosis Chain)</a></li>
-          <li><a target="_blank" href="https://punk.domains">.huwa (HUWA DAO, BSC)</a></li>
-          <li><a target="_blank" href="https://www.kns.earth">.klima (Klima DAO, Polygon PoS Chain)</a></li>
-          <li><a target="_blank" href="https://punk.domains">.L2 (Layer2 DAO, Optimism)</a></li>
-          <li><a target="_blank" href="https://punk.domains">.misie (Misie Community, Arbitrum)</a></li>
-          <li><a target="_blank" href="https://unstoppabledomains.com/">.nft (Unstoppable Domains, Ethereum)</a></li>
-          <li><a target="_blank" href="https://optimistic.domains">.op (Optimistic Domains, Optimism)</a></li>
-          <li><a target="_blank" href="https://punk.domains">.optimism (Punk Domains, Optimism)</a></li>
-          <li><a target="_blank" href="https://punk.domains">.poly (Punk Domains, Polygon PoS Chain)</a></li>
-          <li><a target="_blank" href="https://names.pooly.me">.pool (PoolTogether DAO, Optimism)</a></li>
-          <li><a target="_blank" href="https://ppl.domains">.ppl (People Domains, Arbitrum)</a></li>
-          <li><a target="_blank" href="https://punk.domains/#/punkangel">.punkangel (Punk Domains, Arbitrum)</a></li>
-          <li><a target="_blank" href="https://punk.domains">.safu (Punk Domains, BSC)</a></li>
-          <li><a target="_blank" href="https://id.satraps.io">.satrap (Satrap IDs, Songbird)</a></li>
-          <li><a target="_blank" href="https://sgb.domains">.sgb (Songbird Domains, Songbird)</a></li>
-          <li><a target="_blank" href="https://smol.domains">.smol (Smol Brains NFT, Arbitrum)</a></li>
-          <li><a target="_blank" href="https://punk.domains">.spartan (Punk Domains, Optimism)</a></li>
-          <li><a target="_blank" href="https://punk.domains">.wagmi (Punk Domains, Optimism)</a></li>
-          <li><a target="_blank" href="https://unstoppabledomains.com/">.wallet (Unstoppable Domains, Ethereum)</a></li>
-          <li><a target="_blank" href="https://twb.punk.domains">.wildbunch (The Wild Bunch NFT, Ethereum)</a></li>
-          <li><a target="_blank" href="https://unstoppabledomains.com/">.x (Unstoppable Domains, Ethereum)</a></li>
-          <li><a target="_blank" href="https://punk.domains">.xdai (Punk Domains, Gnosis Chain)</a></li>
-          <li><a target="_blank" href="https://unstoppabledomains.com/">.888 (Unstoppable Domains, Ethereum)</a></li>
-        </ul>
+        <p>Revenue Distributor Frontend is a web3 app that allows you to manage any RevenueDistributor smart contract.</p>
       </div>
     </div>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,148 @@
 <template>
-  <SendTokens />
+  <div class="d-flex justify-content-center">
+    <div class="card text-white bg-primary send-tokens-card">
+      <div class="card-body text-center">
 
-  <Balance />
+        <button
+          v-if="!isActivated" 
+          class="btn btn-primary" 
+          type="button"
+          data-bs-toggle="modal" 
+          data-bs-target="#connectModal"
+        >
+          Connect wallet
+        </button>
 
-  <PaymentLink />
+        <!-- Select network -->
+        <button 
+          v-if="isActivated" 
+          class="btn btn-primary dropdown-toggle" 
+          type="button" 
+          data-bs-toggle="dropdown" 
+          aria-expanded="false"
+        >
+          {{ getChainName(chainId) }}
+        </button>
+        
+        <div class="dropdown-menu p-2 dropdown-menu-end">
+          <div class="mb-3">
+            <input 
+              class="form-control mb-2" 
+              placeholder="Find network"
+              v-model="filterNetwork" 
+            />
+
+            <li>
+              <button 
+                class="dropdown-item" 
+                type="button"
+                v-for="networkName in getNetworks"
+                :key="networkName"
+                @click="changeNetwork(networkName)"
+              >SWITCH TO {{ networkName }}</button>
+            </li>
+          </div>
+        </div>
+        <!-- END Select network -->
+
+        <!-- Recipient -->
+        <div class="mt-4">
+          <input 
+            v-model="distributorAddress"
+            class="form-control form-control-lg text-center"
+            placeholder="Revenue Distributor address"
+          >
+        </div>
+        <!-- END Recipient -->
+
+        <!-- Load button -->
+        <button
+          v-if="isActivated"
+          class="btn btn-lg btn-dark mt-4 mb-2"
+          :disabled="waitingData"
+          @click="loadData"
+        >
+          <span v-if="waitingData" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+          Load Data
+        </button>
+        <!-- END load button -->
+
+        <!-- Connect wallet button -->
+        <button
+          v-if="!isActivated"
+          class="btn btn-lg btn-dark mt-4 mb-2"
+          data-bs-toggle="modal" 
+          data-bs-target="#connectModal"
+        >
+          Connect wallet
+        </button>
+        <!-- END Connect wallet button -->
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import Balance from '../components/Balance.vue';
-import PaymentLink from '../components/PaymentLink.vue';
-import SendTokens from '../components/SendTokens.vue';
+import WaitingToast from "../components/WaitingToast.vue";
+import useChainHelpers from "../composables/useChainHelpers";
+import useDomainHelpers from "../composables/useDomainHelpers";
+import { ethers } from 'ethers';
+import { useBoard, useEthers } from 'vue-dapp';
+import { useToast, TYPE } from "vue-toastification";
 
 export default {
   name: "Home",
 
-  components: {
-    Balance,
-    PaymentLink,
-    SendTokens
+  data() {
+    return {
+      filterNetwork: null,
+      filterTokens: null,
+      distributorAddress: null,
+      waitingData: false
+    }
   },
-  
+
+  created() {
+    this.distributorAddress = this.$route.query.addr; // pass contract address through URL query (e.g. ?addr=0x123...abc)
+  },
+
+  computed: {
+
+    getNetworks() {
+      const networkNames = this.getSupportedChains();
+
+      if (this.filterNetwork) {
+        return networkNames.filter(item => item.includes(this.filterNetwork.toUpperCase())); //filtered
+      }
+
+      return networkNames;
+    }
+  },
+
+  methods: {
+    changeNetwork(networkName) {
+      const networkData = this.switchNetwork(networkName); 
+
+      window.ethereum.request({ 
+        method: networkData.method, 
+        params: networkData.params
+      });
+    }
+  },
+
   setup() {
-    
-  },
+    const { open } = useBoard();
+    const { address, balance, chainId, isActivated, signer } = useEthers();
+    const { getBlockExplorerBaseUrl, getChainName, getSupportedChains, switchNetwork } = useChainHelpers();
+    const { getDomainHolder } = useDomainHelpers();
+    const toast = useToast();
+
+    return { 
+      address, balance, chainId, getBlockExplorerBaseUrl, getChainName, getDomainHolder, getSupportedChains, 
+      isActivated, open, signer, switchNetwork, toast 
+    }
+  }
+
 }
 </script>
